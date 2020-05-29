b97e5d04a94d15f0984c0f1ca993346e9ac08fc4
Saneesh Kumar <saneeshnair@gmail.com>
2020-05-29 20:31:22 +0900

    'Improve'

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", function (req, res) {
 app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
-    console.log(JSON.stringify(req));
+    console.log("Hello World.....");
 
     // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
