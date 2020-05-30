259ff2fd9519728935c9e9588927e0a31df26f59
Saneesh Kumar <saneeshnair@gmail.com>
2020-05-30 16:06:43 +0900

    'Improve'

--- index.js
+++ index.js
@@ -80,6 +80,12 @@ app.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                 text: "Thanks for adding. Please type hi, hello or こんにちは"
             }));
         }
+        else if(event.type === "follow"){
+            events_processed.push(bot.replyMessage(event.replyToken, {
+                type: "text",
+                text: "Sorry to see you going. Please do come back.."
+            }));
+        }
         else{
 
         }
